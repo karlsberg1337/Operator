ce65796f6a9d91dd3066ded393570104cdc4e398
karlsberg1337 <146448442+karlsberg1337@users.noreply.github.com>
2023-12-08 20:49:39 +0700

    Update 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+a = int(input('Введите первое число:'))
+b = int(input('Ввведите второе число:'))
+c = int(input('Введите третье число:'))
+
+if a>b and a>c:
+   print(a)
+elif b>a and b>c:
+   print(b)
+elif c>a and c>b:
+   print(c)
