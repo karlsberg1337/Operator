747a0671a23564cf74c5369b8e4216a569ebb1e4
karlsberg1337 <146448442+karlsberg1337@users.noreply.github.com>
2023-12-08 20:59:15 +0700

    Update 4.py

---
+++ 4.py
@@ -0,0 +1,17 @@
+x = int(input('x: '))
+y = int(input('y: '))
+
+if x > 0 and y > 0:
+         print('1')
+
+elif x < 0 and y > 0:
+        print('2')
+
+elif x < 0 and y < 0:
+        print('3')
+
+elif x > 0 and y < 0:
+
+        print('4')
+else:
+        print('Начало')
